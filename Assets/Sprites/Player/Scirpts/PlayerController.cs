fdc34f4730f363db63362a2616cdd82bc6cc934a
Brandon Dalesandro <brandonadalesandro@gmail.com>
2015-03-20 15:19:27 -0700

    player move/jump

--- Assets/Sprites/Player/Scirpts/PlayerController.cs
+++ Assets/Sprites/Player/Scirpts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	float walkSpeed = 1.0f;
-	float jumpSpeed = 1.0f;
+	float jumpSpeed = 5.0f;
 	protected Animator animator;
 	// Use this for initialization
 	void Start () {
@@ -12,19 +12,19 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		// Update the x according to horizontal input
 		float dx = Input.GetAxisRaw("Horizontal");
 		Vector3 walkVector = new Vector3 (dx, 0, 0) * walkSpeed * Time.deltaTime;
 		animator.SetBool ("isWalk", Mathf.Abs(walkVector.x) > 0);
 		transform.position += walkVector;
-		/*
+
 		// Update the y accoridng to the vertical input
-		float dy = Input.GetAxisRaw("Vertical");
-		animator.SetBool ("isJump", rigidbody2D.velocity.y == 0);
-		if (rigidbody2D.velocity.y == 0) {
+		if (Input.GetKey(KeyCode.UpArrow) && rigidbody2D.velocity.y == 0) {
 			rigidbody2D.AddForce (new Vector2 (0, jumpSpeed), ForceMode2D.Impulse);
 		}
-		*/
+		animator.SetBool ("isJump", Mathf.Abs(rigidbody2D.velocity.y) >= .5);
+
 	}
+
 }
