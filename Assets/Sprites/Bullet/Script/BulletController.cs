672f8b3053c177d2afb2cce54e0665cf55363d11
Brandon Dalesandro <brandonadalesandro@gmail.com>
2015-03-20 17:10:20 -0700

    Added bullet prefab/script, built in shooting in player

---
+++ Assets/Sprites/Bullet/Script/BulletController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	protected Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		// TODO IsName might now work, if so hash graveyard, and check with dobule equals and nameHash
+		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Graveyard")) {
+			//Destroy(gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		if (collision.gameObject.tag == "Enemy") {
+			//animator.SetBool("isImpact" ,true);
+		}
+	}
+}
---
+++ Assets/Sprites/Player/Script/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+	float walkSpeed = 1.0f;
+	float jumpSpeed = 5.0f;
+	float shootSpeed = 2.0f;
+
+	bool isFacingRight = true;
+
+	float lastShootTime = 0;
+	float shootWaitTime = 1;
+
+	Object bulletPrefab;
+
+
+	protected Animator animator;
+	protected List<GameObject> bullets;
+	// Use this for initialization
+	void Start () {
+
+		bulletPrefab = Resources.Load ("Bullet");
+		bullets = new List<GameObject> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Update the x according to horizontal input
+		float dx = Input.GetAxisRaw("Horizontal");
+
+		Vector3 walkVector = new Vector3 (dx, 0, 0) * walkSpeed * Time.deltaTime;
+		if (walkVector.x < 0 && isFacingRight) {
+			flip ();
+		} else if (walkVector.x > 0 && !isFacingRight) {
+			flip ();
+		}
+		animator.SetBool ("isWalk", Mathf.Abs(walkVector.x) > 0);
+		transform.position += walkVector;
+
+		// Update the y accoridng to the vertical input
+		if (Input.GetKey(KeyCode.UpArrow) && rigidbody2D.velocity.y == 0) {
+			rigidbody2D.AddForce (new Vector2 (0, jumpSpeed), ForceMode2D.Impulse);
+		}
+		animator.SetBool ("isJump", Mathf.Abs(rigidbody2D.velocity.y) >= .5);
+
+	}
+
+	void Update () {
+		bool isShoot = animator.GetBool("isShoot");
+		if (isShoot) animator.SetBool("isShoot", false);
+
+		
+		if (Input.GetKey (KeyCode.Space) && !isShoot && Time.time - lastShootTime >= shootWaitTime) {
+			animator.SetBool("isShoot", true);
+			lastShootTime = Time.time;
+
+			// TODO set the correct position for the bullet to spawn
+			GameObject go = Instantiate(Resources.Load("Bullet", typeof(GameObject))) as GameObject;
+			go.transform.position = transform.position;
+			Vector2 bulletForce = new Vector2(isFacingRight ? shootSpeed : -shootSpeed, 0);
+			go.rigidbody2D.AddForce(bulletForce, ForceMode2D.Impulse);
+			bullets.Add (go);
+		}
+	}
+
+	void flip() {
+		isFacingRight = !isFacingRight;
+		
+		Vector3 scalar = transform.localScale;
+		scalar.x *= -1;
+		transform.localScale = scalar;
+	}
+
+}
