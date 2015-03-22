4714b5c3d640a948a00185be71af3bfaa299ca14
Brandon Dalesandro <brandonadalesandro@gmail.com>
2015-03-21 22:36:38 -0700

    capture zones intial done

---
+++ Assets/Sprites/CaptureZone/Script/CaptureZoneController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaptureZoneController : MonoBehaviour {
+
+	public float timeToCapture = 2.0f;
+	public float zoneRegenRate = .5f;
+	float timeLeft;
+
+	bool isPlayerHere = false;
+
+	// Use this for initialization
+	void Start () {
+		timeLeft = timeToCapture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( timeLeft <= 0) {
+			Destroy(gameObject);
+		}
+
+		if (isPlayerHere) {
+			timeLeft -= Time.deltaTime;
+		} else {
+			timeLeft = Mathf.Clamp(timeLeft + zoneRegenRate, 0, timeToCapture);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collision){
+		if (collision.gameObject.tag == "Player") {
+			isPlayerHere = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collision) {
+		if (collision.gameObject.tag == "Player") {
+			isPlayerHere = false;
+		}
+	}
+}
